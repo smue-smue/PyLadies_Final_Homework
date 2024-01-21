a663feab66d90949538839ef6d45b31248539413
Sandra Müllner <s_mue_@outlook.com>
2024-01-21 17:14:16 +0100

    Renamed parseds arguments.

--- PyL_13_ho.py
+++ PyL_13_ho.py
@@ -5,8 +5,8 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="argparse greeting")
-parser.add_argument("--input", help="input file", required=True)
-parser.add_argument("--output", help="output file", required=True)
+parser.add_argument("--in_file", help="input file", required=True)
+parser.add_argument("--out_file", help="output file", required=True)
 parser.add_argument("--offset", help="offset value", required=True)
 parser.add_argument("--replacing_character", help="old character to be replaced", required=True)
 parser.add_argument("--replacement_number", help="number to replace old character", required=True)
@@ -14,14 +14,14 @@ parser.add_argument("--indent", action="store_true", help="name will be indented
 
 # 1st changes - ceasars cipher, moving characters by a given offset
 
-def cipher_ceasar(input, offset):
+def cipher_ceasar(in_file, offset):
     """Program that opens a file and saves the changes to another file."""
 
     int_offset = int(offset)
 
     ciphered_ceasar = ""
 
-    with open(input, encoding="utf-8") as input_file:
+    with open(in_file, encoding="utf-8") as input_file:
         for line in input_file:
             new_line = ""
             for char in line:
@@ -58,8 +58,8 @@ def cipher_number(result_ciphered_ceasar, replacing_character, replacement_numbe
 
 # Saving the final version to a new file
 
-def save_to_new_file(result_cipher_number, output):
-    with open(output, mode="w", encoding="utf-8") as output_file:
+def save_to_new_file(result_cipher_number, out_file):
+    with open(out_file, mode="w", encoding="utf-8") as output_file:
         output_file.write(result_cipher_number)
     output_file.close()
 
@@ -67,11 +67,11 @@ def save_to_new_file(result_cipher_number, output):
 
 args = parser.parse_args()
 
-result_cipher_ceasar = cipher_ceasar(args.input, args.offset) # result is a string
+result_cipher_ceasar = cipher_ceasar(args.in_file, args.offset) # result is a string
 
 result_cipher_number = cipher_number(result_cipher_ceasar, args.replacing_character, args.replacement_number) # result is a string
 
-save_to_new_file(result_cipher_number, args.output)
+save_to_new_file(result_cipher_number, args.out_file)
 
 print("\nCiphered Ceasar: \n")
 print(result_cipher_ceasar)
